54993dff24c3bd88fc3d6fa47f28d442376ecf6e
mufasa1976 <mufasa1976@coolstuff.software>
2018-08-09 08:19:51 +0200

    initial commit

---
+++ src/main/java/io/github/mufasa1976/springframework/jsf/primefaces/JsfPrimefacesApplication.java
@@ -0,0 +1,12 @@
+package io.github.mufasa1976.springframework.jsf.primefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfPrimefacesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JsfPrimefacesApplication.class, args);
+  }
+}
---
+++ src/test/java/io/github/mufasa1976/springframework/jsf/primefaces/JsfPrimefacesApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.mufasa1976.springframework.jsf.primefaces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsfPrimefacesApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
